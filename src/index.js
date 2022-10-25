b5b32681d989949581417134667fc4fc66ae3802
NicolleAmr <86444373+NicolleAmr@users.noreply.github.com>
2022-10-25 11:01:41 -0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -5,18 +5,37 @@ const porta = process.env.PORT || 4040
 app.use(express.json())
 app.set('view engine', 'ejs')
 
+//multer - pacote dentro do modulo que faz dowload dentro da plataforma
+const multer = require('multer')
+
+//storage
+const storage = multer.diskStorage({
+    //destino
+    destination: (req,res,cb) =>{
+        cb(null,'uploads')
+    },
+
+    //nome do arquivo
+    filename: (req,file, cb) =>{
+        cb(null,file.originalname)
+    }
+})
+
+const upload = multer ({storage})
+
 //rota padrão
 app.get('/',(req,res)=>{
     res.render('index.ejs')
 })
 
 //upload
-app.post('/', (req,res)=>{
+//midlware - procurar dps o que ele faz, pq o loko n explica direito
+app.post('/', upload.single('arquivo'),(req,res)=>{
     res.send("LOL")
     console.log(req.body,req.file) //apoio a lógica
 })
 
 //escuta do servidor
 app.listen(porta, () =>{
-    console.log(`Servidor escutanto em http:\\localhost:${porta}`)
+    console.log(`Servidor escutanto em http://localhost:${porta}`)
 })
